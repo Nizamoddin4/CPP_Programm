42934293db40a00db13fc14ed07b3270723beab8
Nizamoddin4 <nizamoddin1110@gmail.com>
2020-09-08 20:56:36 +0530

    adding changes to output

--- 5.cpp
+++ 5.cpp
@@ -4,4 +4,5 @@ int main()// From this point program starts
 {
 	cout<<"This is hello world program";// Writing to output stream.
 	return 0;// Returning integer value (Here it is optional)
+	cout<<"This line is added to check staging";
 }
